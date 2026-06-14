031065da0a619fffe7810209f0a3156ece877bab
Khoiru <jaebelabidin@gmail.com>
2026-06-14 13:23:24 +0700

    Update deploy.sh to alter database user password if it already exists

--- deploy.sh
+++ deploy.sh
@@ -77,6 +77,7 @@ systemctl enable mysql
 echo -e "${BIRU}Membuat database ${DB_NAME} dan user ${DB_USER}...${NC}"
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
 mysql -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';"
+mysql -e "ALTER USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'localhost';"
 mysql -e "FLUSH PRIVILEGES;"
 
