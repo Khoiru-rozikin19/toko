5792eb274a75ae6ee9cd3d55edd0a1e7930f6bbb
Khoiru <jaebelabidin@gmail.com>
2026-06-17 12:30:25 +0700

    card saldo v2

--- resources/views/balance.blade.php
+++ resources/views/balance.blade.php
@@ -29,7 +29,7 @@
     <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
         
         <!-- Current Balance Card -->
-        <div class="bg-linear-to-br from-blue-600 to-indigo-700 dark:from-blue-700 dark:to-indigo-900 rounded-3xl p-6 text-white shadow-xl shadow-blue-500/15 flex flex-col justify-between relative overflow-hidden h-52 lg:col-span-2 border border-blue-500/10 dark:border-blue-400/20">
+        <div class="rounded-3xl p-6 text-white shadow-xl shadow-blue-500/15 flex flex-col justify-between relative overflow-hidden h-52 lg:col-span-2 border border-blue-500/10 dark:border-blue-400/20" style="background: linear-gradient(to bottom right, #2563eb, #4338ca);">
             <!-- Background Decoration -->
             <div class="absolute -right-10 -top-10 w-40 h-40 bg-white/10 rounded-full blur-2xl"></div>
             <div class="absolute -left-10 -bottom-10 w-36 h-36 bg-blue-500/30 rounded-full blur-2xl"></div>
