2d836c090fe49ca75738b10c2d637560c5110784
Khoiru <jaebelabidin@gmail.com>
2026-07-12 14:15:09 +0700

    feat: initialize database seeders with default admin, system settings, and product categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,14 +30,13 @@ class DatabaseSeeder extends Seeder
         // 2. Create Initial Settings (Real-format static QRIS mockup and API Key)
         Setting::set(
             'qris_static_string', 
-            '00020101021126610014COM.GO-JEK.WWW01189360091430224274230210G0224274230303UMI51440014ID.CO.QRIS.WWW0215ID10243581829610303UMI5204573253033605802ID5919Rzk store, SNR PNNJ6003OKU61053215962070703A016304881F'
+            '00020101021126570011ID.DANA.WWW011893600915302634402802090263440280303UMI51440014ID.CO.QRIS.WWW0215ID10265391682640303UMI5204481453033605802ID5908rz store6015Kab. Ogan Komer610532159630485BE'
         );
         Setting::set('api_secret_key', 'rahasiahappy123');
 
         // 3. Create Categories
         $kuotaXl = \App\Models\Category::updateOrCreate(['name' => 'Kuota XL']);
         $digital = \App\Models\Category::updateOrCreate(['name' => 'Digital']);
-        $topUpGame = \App\Models\Category::updateOrCreate(['name' => 'Top up gaame']);
 
     }
 }
